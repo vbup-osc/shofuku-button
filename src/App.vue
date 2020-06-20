113e597bc95e8eb4f245fd43c1becde76ddf20bf
MioButton <samuimomijiyuki@qq.com>
2020-06-20 18:47:56 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -60,7 +60,7 @@
 @import "../node_modules/bootstrap/dist/css/bootstrap.css";
 .main-content{ /*内容部分*/
     min-height: 100vh;
-    background-color: #e9e5ff;
+    background-color: #f4f2ff;
 }
 .btn-buttom{
     border: 0px;
