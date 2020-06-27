50fdd2f068fef9f5e43af1a90a284666825b9a9d
MioButton <samuimomijiyuki@qq.com>
2020-06-27 22:03:37 +0800

    ios

--- src/App.vue
+++ src/App.vue
@@ -18,15 +18,15 @@
                         <li><a href="https://twitter.com/Nyako_Shofuku?s=09" target="_blank"><img src="resources/Twitter_Social_Icon_Circle_Color.png" height="27"/></a></li>
                         <li><a href="https://space.bilibili.com/17855498" target="_blank"><img src="resources/bilibili.png" height="27"/></a></li>
                     </ul>
-                    <!--<ul class="nav navbar-nav navbar-right" style="margin-top:0px;margin-bottom:0px">
+                    <ul class="nav navbar-nav navbar-right" style="margin-top:0px;margin-bottom:0px">
                         <li class="dropdown">
                             <a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><img src="resources/download.svg" height="23"/></a>
                             <ul class="dropdown-menu">
                                 <li style="width: 63px;"><a href="https://www.lanzous.com/ib3yeob" target="_blank" style="padding-left:18px"><img src="resources/Android.png" height="27"/></a></li>
-                                <li style="width: 63px;"><a href="https://www.lanzous.com/iarrhef" target="_blank" style="padding-left:18px"><img src="resources/Apple.png" height="27"/></a></li>
+                                <li style="width: 63px;"><a href="https://nyakoshofuku.org/resources/apple.mobileconfig" download="apple.mobileconfig" target="_blank" style="padding-left:18px"><img src="resources/Apple.png" height="27"/></a></li>
                             </ul>
                         </li>
-                    </ul>-->
+                    </ul>
                     <ul class="nav navbar-nav navbar-right" style="margin-top:0px;">
                         <li class="dropdown">
                             <a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><img src="resources/lang.svg" height="23"/></a>
